15457fa40e190cc1b183c62e0c06f2152b7da09c
Mohak-Singhal <mohaksinghal516@gmail.com>
2025-09-01 18:35:32 +0530

    Update app.js

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.use("/applicants", adminPanelRoutes);
 app.use("/applicants", finalsheetRouter);
 
 // Start sync cron
-// startSyncCron();
+startSyncCron();
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
